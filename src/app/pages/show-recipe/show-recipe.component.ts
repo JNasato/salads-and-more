9ee65758d393df4ffd594c97a50b727f454d94fc
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-08-21 01:16:42 -0700

    add logic and styling to recipe show page

--- src/app/pages/show-recipe/show-recipe.component.ts
+++ src/app/pages/show-recipe/show-recipe.component.ts
@@ -1,21 +1,47 @@
 import { RecipeService } from './../../services/recipe.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { Recipe } from 'src/app/classes/recipe';
 
 @Component({
   selector: 'app-show-recipe',
   templateUrl: './show-recipe.component.html',
   styleUrls: ['./show-recipe.component.scss']
 })
-export class ShowRecipeComponent implements OnInit {
+export class ShowRecipeComponent implements OnInit, OnDestroy {
+
+  private subscriptons: Subscription[] = [];
+  public recipe: Recipe;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private location: Location
   ) { }
 
   ngOnInit(): void {
+    this.subscriptons.push(
+      this.route.paramMap.subscribe(params => {
+        const recipeId = params.get('id');
+        this.recipe = this.recipeService.getRecipeById(parseInt(recipeId));
+      })
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptons.forEach(sub => sub.unsubscribe());
+  }
+
+  back() {
+    this.location.back();
+  }
+
+  deleteRecipe(): void {
+    this.recipeService.deleteRecipe(this.recipe.id);
+    this.router.navigate(['']);
   }
 
 }
